cc4be8a8fc9c5072b6b3106fc47f4d17e822a907
AminSaedi <amin.di.sam@gmail.com>
2021-05-07 12:47:40 +0430

    Move project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Preson";
-
+import styled from "styled-components";
 function App() {
   const [persons, setPersons] = useState([
     { id: 1, name: "Amin", age: 18 },
     { id: 2, name: "Farnaz", age: 16 },
   ]);
+  const [showPersons, setShowPersons] = useState(false);
   const handleDeletePerson = (personId) => {
     let localPersons = [...persons];
     const personIndex = localPersons.findIndex((p) => p.id === personId);
@@ -14,8 +15,8 @@ function App() {
       return index !== personIndex;
     });
     setPersons(localPersons);
+    const handleChangePerson = (personId, event) => {
   };
-  const handleChangePerson = (personId, event) => {
     const localPersons = [...persons];
     const person = localPersons.find((p) => p.id === personId);
     const personIndex = localPersons.findIndex((p) => p.id === personId);
@@ -23,17 +24,35 @@ function App() {
     localPersons[personIndex] = person;
     setPersons(localPersons);
   };
+  const StyledButton = styled.button`
+    background-color: ${(props) => props.alt ? "blue" : "green"};
+    color: white;
+    border: 0;
+    font-size: 3rem;
+    margin-top: 2rem;
+    &:hover {
+      background-color: red;
+    }
+  `;
+  // if (showPersons) buttonStyle.backgroundColor = "green";
   return (
     <div className="App">
-      {persons.map((person) => (
-        <Person
-          key={person.id}
-          name={person.name}
-          age={person.age}
-          handleChange={(event) => handleChangePerson(person.id, event)}
-          handleDelete={() => handleDeletePerson(person.id)}
-        />
-      ))}
+      <StyledButton
+       alt={showPersons}
+       onClick={() => setShowPersons(!showPersons)}
+      >
+       Show Persons
+      </StyledButton>
+      {showPersons &&
+       persons.map((person) => (
+         <Person
+           key={person.id}
+           name={person.name}
+           age={person.age}
+           handleChange={(event) => handleChangePerson(person.id, event)}
+           handleDelete={() => handleDeletePerson(person.id)}
+         />
+       ))}
     </div>
   );
 }
--- src/Person/Preson.jsx
+++ src/Person/Preson.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./Preson.css";
+import styled from "styled-components";
 
 const Person = ({ name, age, handleChange, handleDelete }) => {
+  const StyledDiv = styled.div`
+    width: 60%;
+    margin: 2rem auto;
+    padding: 2rem 0;
+    border: 2px solid dodgerblue;
+    :hover {
+      background-color: azure;
+      font-size: 2rem;
+    }
+  `;
   return (
-    <div className="Person" onDoubleClick={handleDelete}>
+    <StyledDiv onDoubleClick={handleDelete}>
       <p>
         My name is {name} and i'm {age} years old.
       </p>
       <input value={name} onChange={handleChange} />
-    </div>
+    </StyledDiv>
   );
 };
 
